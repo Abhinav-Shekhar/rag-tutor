2adf544a34b43a91478909bca4f26cb4db81175d
Abhinav-Shekhar <49090890+Abhinav-Shekhar@users.noreply.github.com>
2026-05-08 01:35:05 +0530

    Update model app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ api_key = st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 # ============================================================
 # LEARNER PROFILE
