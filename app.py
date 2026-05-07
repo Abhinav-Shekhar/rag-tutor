c93be3b8aa99e5172ed29e9e8ee9c5de4de5fb8f
Abhinav-Shekhar <49090890+Abhinav-Shekhar@users.noreply.github.com>
2026-05-08 01:02:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 # ============================================================
 # PAGE CONFIG
